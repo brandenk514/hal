3d540a8cce1de95fd510d11e59467755f1011b57
Branden <brandenk514@gmail.com>
2017-01-15 12:49:27 -0600

    Worked on parsing for sentences

--- speech.py
+++ speech.py
@@ -1,5 +1,6 @@
 import speech_recognition
 from os import system
+from array import array
 
 recognizer = speech_recognition.Recognizer()
 
@@ -18,10 +19,22 @@ def ping(ip):
     system("Ping " + ip)
 
 
+def parse(audio):
+    words = array('i')
+    print(type(words))
+    print(audio)
+    print(len(audio))
+    for a in range(len(audio)):
+        if a != " ":
+            words.remove('i')
+            words.insert(0, a)
+    print(words)
+
+
 def listen():
     with speech_recognition.Microphone() as source:
         recognizer.adjust_for_ambient_noise(source)
-        audio = recognizer.listen(source)
+        audio = recognizer.listen(source, 5000)
 
     try:
         return recognizer.recognize_wit(audio, "PYEBZJDFJJTY7J6RN4CVFWTO7DLYK5Y6")
@@ -29,10 +42,11 @@ def listen():
         print("Could not understand audio")
     except speech_recognition.RequestError as e:
         print("Recognition Error; {0}".format(e))
+    except speech_recognition.WaitTimeoutError:
+        print("Are you there?")
 
     return ""
 
 
 if __name__ == '__main__':
-    print(listen())
-    speak("How are you doing today, " + name)
+    parse(listen())
