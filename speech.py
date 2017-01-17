19831cf11cfc19ed7130363b5198ba783ed5731c
Branden <brandenk514@gmail.com>
2017-01-17 16:19:48 -0600

    Finished up open_app method

--- speech.py
+++ speech.py
@@ -1,16 +1,45 @@
 import speech_recognition
+import os
 from os import system
 import re
 import unicodedata
 
 recognizer = speech_recognition.Recognizer()
 
+
 def speak(text):
     system("Say " + text)
 
 
-def openApp(app):
-    system("Open /Applications/" + app + ".app")
+def index_directory(directory):
+    app_list = os.listdir(directory)
+    app_list.sort()
+    return app_list
+
+
+def open_app(application):
+    selected_app = application.capitalize()
+    directory = "/Applications/"
+    apps = index_directory(directory)
+    folders = []
+    for a in apps:
+        if not a.endswith(".app"):
+            folders.append(a)
+            apps.remove(a)
+
+    if selected_app in apps:
+        system("Open " + directory + selected_app + ".app")
+        return None
+    else:
+        if selected_app in folders:
+            select_dir = index_directory(directory + selected_app)
+            cur = selected_app + ".app"
+            if cur in select_dir:
+                system("Open " + directory + selected_app + "/" + cur)
+                return None
+            else:
+                print("Application not found")
+                return None
 
 
 def ping(ip):
@@ -26,17 +55,16 @@ def parse(audio):
 def listen():
     with speech_recognition.Microphone() as source:
         recognizer.adjust_for_ambient_noise(source)
-        audio = recognizer.listen(source, 5000)
-
+        audio = recognizer.listen(source, 5)
     try:
         return recognizer.recognize_wit(audio, "PYEBZJDFJJTY7J6RN4CVFWTO7DLYK5Y6")
     except speech_recognition.UnknownValueError:
         print("Could not understand audio")
     except speech_recognition.RequestError as e:
-        print("Recognition Error; {0}".format(e))
+        print("Recognition Error: {0}".format(e))
 
     return ""
 
 
 if __name__ == '__main__':
-    print(parse(listen()))
+    open_app('Cisco')
